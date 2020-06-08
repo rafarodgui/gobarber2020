691d8c943fd15d5ba3104c5a714fa1126b51c392
rafaelRodruiguesGuimaraes <rafarodgui@hotmail.com>
2020-06-08 01:08:17 -0300

    first frontend commit

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { getRepository } from 'typeorm';
 import uploadConfig from '../config/upload';
 import UpdateUserAvatarService from '../Services/UpdateUserAvatarService';
 
 import CreateUserService from '../Services/CreateUserService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+import User from '../models/User';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -20,6 +23,14 @@ usersRouter.post('/', async (request, response) => {
   delete user.password;
 
   return response.json({ user });
+});
+
+usersRouter.get('/', async (request, response) => {
+  const userRepository = getRepository(User);
+
+  const users = await userRepository.find();
+
+  return response.json(users);
 });
 
 usersRouter.patch(
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 
+import cors from 'cors';
+
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
@@ -10,6 +12,7 @@ import AppError from './errors/AppError';
 import './database';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
