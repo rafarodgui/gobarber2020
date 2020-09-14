3570c4ae7ab979093d0436cbdaf50cb2fbf4a032
Rafael Guimarães <rafarodgui@hotmail.com>
2020-09-14 11:21:30 -0300

    Reset password finished

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -10,7 +10,7 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepositories';
 
 import IUserTokensRepository from '@modules/users/repositories/IUserTokenRepository';
-import UserTokensRepository from '@modules/users/infra/typeorm/repositories/';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
@@ -20,4 +20,9 @@ container.registerSingleton<IAppointmentsRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
