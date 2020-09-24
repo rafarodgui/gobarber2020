cc99f230c75d1f19f0e231747b7aa83cf3476ccf
Rafael Guimarães <rafarodgui@hotmail.com>
2020-09-24 17:47:35 -0300

    Find all hours in a day from a provider

--- backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -8,4 +8,5 @@ export default interface IAppointmentsRepository {
   findAllInMonthFromProvider(
     date: IFindAllInMonthFromProviderDTO,
   ): Promise<Appointment[]>;
+  findAllinDayFromProvider();
 }
---
+++ backend/src/modules/appointments/services/FindAllInDayFromProvider.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import { injectable, inject } from 'tsyringe';
+
+// import { getDaysInMonth, getDate } from 'date-fns';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  hour: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderMonthAvailability {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    return [{ hour: 1, available: false }];
+  }
+}
+
+export default ListProviderMonthAvailability;
